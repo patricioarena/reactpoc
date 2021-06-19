3677699b24f15ef319899c93be1c6cc98db6f81f
patricioarena <patricio.e.arena@gmail.com>
2021-06-19 04:01:00 -0300

    fix: http to https

--- src/Components/Enviroment.js
+++ src/Components/Enviroment.js
@@ -1,14 +1,9 @@
 import React, { useRef, useState } from "react"
 
-
-
-
-
 function EnviromentCustom() {
 
   const [node_env, setEnv] = useState(process.env.NODE_ENV)
   window.process = node_env
-
 
   return (
     <div className="ribbon-box">
